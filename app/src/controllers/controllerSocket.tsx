c47f2d3092e0329b5221a68877c2c03ae61198f8
xWalian <lukasek008@gmail.com>
2024-12-01 23:03:42 +0100

    Optimize frame updates and replace FastImage with Image
    Introduce a frame update interval to reduce redundancy and improve performance by updating the image source only every few frames. Replace FastImage with the native Image component for consistency and adjust the image width for improved layout.

--- app/src/controllers/controllerSocket.tsx
+++ app/src/controllers/controllerSocket.tsx
@@ -17,20 +17,22 @@ const VideoClient = () => {
     useEffect(() => {
         const socket = dgram.createSocket({ type: 'udp4' });
 
-//         socket.setRecvBufferSize(10 * 1024 * 1024);  // Set buffer size to 10MB
+        // socket.setRecvBufferSize(10 * 1024 * 1024);  // Set buffer size to 10MB
 
         socket.bind(port, () => {
             console.log(`UDP socket listening on ${host}:${port}`);
         });
 
-        socket.on('message', (msg, rinfo) => {
-            if (msg.length > 0) {
-                const base64String = `data:image/jpeg;base64,${Buffer.from(msg).toString('base64')}`;
+        let frameUpdateInterval = 5;  // Co ile klatek aktualizować
+        let receivedFrames = 0;
 
-                // Only update the image source if it's a new frame (to avoid unnecessary updates)
+        socket.on('message', (msg, rinfo) => {
+            receivedFrames++;
+            if (receivedFrames % frameUpdateInterval === 0) {
+                const base64String = `data:image/jpeg;base64,${Buffer.from(msg).toString('base64')}`;
                 if (base64String !== prevFrameRef.current) {
                     prevFrameRef.current = base64String;
-                    setImageSource(base64String);  // Set the new image source
+                    setImageSource(base64String);
                     setFrameCount((prevCount) => prevCount + 1);
                 }
             }
@@ -45,12 +47,10 @@ const VideoClient = () => {
     return (
         <View style={styles.container}>
             {imageSource && (
-                <FastImage
+                <Image
                     source={{ uri: imageSource }}
                     style={styles.image}
                     resizeMode={FastImage.resizeMode.cover}  // Similar to the 'cover' option in Image
-                    shouldRasterizeIOS={true}  // For iOS performance optimization
-                    priority={FastImage.priority.normal}  // Optional, set the priority (normal, low, high)
                 />
             )}
             <Text style={styles.counterText}>
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     image: {
-        width: 500,        // Fixed width
+        width: 400,        // Fixed width
         height: 400,       // Fixed height
         opacity: 1,        // Ensure no fade-out effect
         transform: [],     // No scaling or other transforms
